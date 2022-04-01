5c501d78a82ab5a48e1aae6b9a909978c103346c
Carlos Mario Perez Castilla <cm.perez@uniandes.edu.co>
2022-04-01 16:49:27 -0500

    casi listo

--- grafosConPesos.py
+++ grafosConPesos.py
@@ -1,4 +1,5 @@
 from collections import deque
+import functools
 
 
 class Grafo(object):
@@ -60,7 +61,7 @@ def dijkstra(grafo, destino, etiquetas, procesados):
 def generarEtiqueta(grafo, nodo, anterior, etiquetas):
     etiquetaNodoAnterior = etiquetas[anterior]
     etiquetaPropuesta = peso(grafo, anterior, nodo) + acumulado(etiquetaNodoAnterior), anterior
-    if (not (etiquetas.has_key(nodo)) or acumulado(etiquetaPropuesta) < acumulado(etiquetas[nodo])):
+    if (not (nodo in etiquetas) or acumulado(etiquetaPropuesta) < acumulado(etiquetas[nodo])):
         etiquetas.update({nodo: etiquetaPropuesta})
 
 
@@ -69,12 +70,12 @@ def aristas(grafo, nodo):
 
 
 def vecinoNoProcesado(grafo, nodo, procesados):
-    aristasDeVecinosNoProcesados = filter(lambda x: not x in procesados, aristas(grafo, nodo))
+    aristasDeVecinosNoProcesados = [x for x in aristas(grafo, nodo) if not x in procesados]
     return [arista.elemento for arista in aristasDeVecinosNoProcesados]
 
 
 def peso(grafo, nodoOrigen, nodoDestino):
-    return reduce(lambda x, y: x if x.elemento == nodoDestino else y, grafo.relaciones[nodoOrigen]).peso
+    return functools.reduce(lambda x, y: x if x.elemento == nodoDestino else y, grafo.relaciones[nodoOrigen]).peso
 
 
 def acumulado(etiqueta):
@@ -86,5 +87,37 @@ def anterior(etiqueta):
 
 
 def menorValorNoProcesado(etiquetas, procesados):
-    etiquetadosSinProcesar = filter(lambda nodo, _: not nodo in procesados, etiquetas.items())
-    return min(etiquetadosSinProcesar, key=lambda _, acum, __: acum)[0]
+    etiquetadosSinProcesar = [nodo__ for nodo__ in iter(etiquetas.items()) if not nodo__[0] in procesados]
+    return min(etiquetadosSinProcesar, key=lambda __acum___: __acum___[1][0])[0]
+
+a = "11"
+b = "12"
+c = "13"
+d = "21"
+e = "22"
+f = "23"
+g = "31"
+h = "32"
+ix = "33"
+grafo = Grafo()
+agregar(grafo, a)
+agregar(grafo, b)
+agregar(grafo, c)
+agregar(grafo, d)
+agregar(grafo, e)
+agregar(grafo, f)
+agregar(grafo, g)
+agregar(grafo, h)
+agregar(grafo, ix)
+relacionar(grafo, a, b, 1)
+relacionar(grafo, b, c, 1)
+relacionar(grafo, d, e, 1)
+relacionar(grafo, b, g, 5)
+relacionar(grafo, c, f, 5)
+relacionar(grafo, c, d, 2)
+relacionar(grafo, d, f, 2)
+relacionar(grafo, d, e, 4)
+relacionar(grafo, e, h, 1)
+relacionar(grafo, e, g, 2)
+relacionar(grafo, f, h, 3)
+print (caminoMinimo(grafo, a, c))
